16626ccccfb991eb29f7854c7d6c319b10299f5d
tnayak <tuna00001@stud.uni-saarland.de>
2023-06-18 22:48:27 +0200

    project simclr commit

--- helpers/pValAnalysis.py
+++ helpers/pValAnalysis.py
@@ -1,3 +1,5 @@
+#pValAnalysis.py
+
 from random import sample
 import numpy as np
 import torch 
@@ -6,83 +8,23 @@ import os
 from traitlets import observe
 from helpers.CustomAudioDataset import CustomAudioDataset
 
-from torch.utils.data import DataLoader
-
-from helpers.metrics import audMetrics
-
-
-def evaluate_Pvalue(model_1, model_2, transform_1, transform_2, opt_1, opt_2, loss, SR, batch_size, epochs, num_runs, device):
-
-    model_empty_1 = model_1
-    model_empty_2 = model_2
-
-    trainDataSet_1, testDataset_1 = getDataSet(SR, transform_1)
-    trainDataSet_2, testDataset_2 = getDataSet(SR, transform_2)
-    
-    trainDataSet_1_x, trainDataSet_1_y = np.array(trainDataSet_1)[:, 0], np.array(trainDataSet_1)[:, 1]
-    trainDataSet_2_x, trainDataSet_2_y = np.array(trainDataSet_2)[:, 0], np.array(trainDataSet_2)[:, 1]
-    
-    print("Training Model-1")
-
-    model_1 = learn(model_1, loss, opt_1, trainDataSet_1_x, trainDataSet_1_y, device, epochs, batch_size)
-    metrics_dict_1 = test(model_1, testDataset_1, batch_size, device)
-    
-    print("Training Model-2")
-    
-    model_2 = learn(model_2, loss, opt_2, trainDataSet_2_x, trainDataSet_2_y, device, epochs, batch_size)
-    metrics_dict_2 = test(model_2, testDataset_2, batch_size, device)
-    
-    observe_diff   = metrics_dict_1['accuracy'] - metrics_dict_2['accuracy']
-
-    print(f" observe_diff between {model_1.__class__.__name__} and {model_2.__class__.__name__} : {observe_diff}")
-
-    diffs = []
-    
-    for i in range(num_runs):
-        x_boot_1, y_boot_1, x_boot_2, y_boot_2  = getBootstrapSample(trainDataSet_1, trainDataSet_2)
-
-        print(f'Training Bootstapped Model-1 Sample#{i+1}')
-        model_1 = learn(model_empty_1, loss, opt_1, x_boot_1, y_boot_1, device, epochs, batch_size)
-        metrics_dict_1 = test(model_1, testDataset_1, batch_size, device)
-
-        print(f'Training Bootstapped Model-2 Sample#{i+1}')
-        model_2 = learn(model_empty_2, loss, opt_2, x_boot_2, y_boot_2, device, epochs, batch_size)
-        metrics_dict_2 = test(model_2, testDataset_2, batch_size, device)
-
-        sample_diff   = metrics_dict_1['accuracy'] - metrics_dict_2['accuracy']
-
-        diffs.append(sample_diff)
-
-        print(f"sample difference between models for bootstrap {i} : {sample_diff}")
-
-    print(diffs)
-
-    p_value = (np.sum(diffs >= observe_diff) + np.sum(diffs <= -observe_diff)) / num_runs
-
-    return p_value
-
 def getDataSet(sr, transform):
     PARENT_PTH = os.getcwd()
     TEST_PTH = os.path.join(PARENT_PTH, 'data', 'test')
     TRAIN_PTH = os.path.join(PARENT_PTH, 'data', 'train')
     
     trainDataSet = CustomAudioDataset(TRAIN_PTH, sr, transform)
-    testDataset  = CustomAudioDataset(TEST_PTH, sr, transform)
+    testDataset = CustomAudioDataset(TEST_PTH, sr, transform)
 
     return trainDataSet, testDataset
 
 def toTensor(X_train):
     tot_samples = X_train.shape[0]
-    tensor_shape = X_train[0].shape
-    if len(tensor_shape)==2:
-        a,b = tensor_shape
-        inp_flat    = np.zeros((tot_samples, a,b))
-    if len(tensor_shape)==3:
-        a,b,c = tensor_shape
-        inp_flat    = np.zeros((tot_samples, a,b,c))
+    a, b        = X_train[0].shape
+    inp_flat    = np.zeros((tot_samples, a*b))
 
     for i, item in enumerate(X_train):
-        inp_flat[i] =  np.array(item)
+        inp_flat[i, :] =  np.array(item).reshape(-1, )
 
     return torch.from_numpy(inp_flat)
 
@@ -99,92 +41,102 @@ def getBootstrapSample( trainDataSet_1, trainDataSet_2, replace = True):
 
     x_len            = trainDataSet_1_x.shape[0]
 
-    sample_ind = np.random.choice(x_len, size = x_len, replace = replace)  
+    sample_ind = np.random.choice(x_len, size = x_len, replcae = replace)  
 
     x_boot_1   = trainDataSet_1_x[sample_ind, :]
-    y_boot_1   = trainDataSet_1_y[sample_ind]
+    y_boot_1   = trainDataSet_1_y[sample_ind, :]
 
     x_boot_2   = trainDataSet_2_x[sample_ind, :]
-    y_boot_2   = trainDataSet_2_y[sample_ind]
+    y_boot_2   = trainDataSet_2_y[sample_ind, :]
 
     return x_boot_1, y_boot_1, x_boot_2, y_boot_2
+
+def evaluate_Pvalue(model_1, model_2, transform_1, transform_2, opt_1, opt_2, loss, SR, batch_size, epochs, num_runs, device):
+
+    model_empty_1 = model_1
+    model_empty_2 = model_2
+
+    trainDataSet_1, testDataset_1 = getDataSet(SR, transform_1)
+    trainDataSet_2, testDataset_2 = getDataSet(SR, transform_2)
     
-def test(model, testDataset, batch_size, device):
+    model_1 = learn(model_1, loss, opt_1, trainDataSet_1, device, epochs)
+    model_2 = learn(model_2, loss, opt_2, trainDataSet_2, device, epochs)
 
-    model.eval()
-
-    testLoader = DataLoader(testDataset, batch_size  = batch_size, shuffle = True)
-    tot_batches  = len(testLoader)
-    lenDataSet   = len(testLoader)
-    np_preds     = np.zeros((len(testDataset)))
+    metrics_dict_1 = test(model_1, testDataset_1)
+    metrics_dict_2 = test(model_2, testDataset_2)
     
-    metrics_dict = None
+    observe_diff   = metrics_dict_1['accuracy'] - metrics_dict_2['accuracy']
 
-    runn_acc    = 0
-    runn_prec   = 0
-    runn_rec    = 0
-    runn_f1     = 0
+    diffs = []
 
-    with torch.no_grad():
-        for i, (aud, labels) in enumerate(testLoader, start=0):
-            aud      = aud.to(device)
-            labels   = labels.to(device)
-            outputs  = model(aud)
-            _, preds = torch.max(outputs, 1)
+    for i in range(num_runs):
+        x_boot_1, y_boot_1, x_boot_2, y_boot_2  = getBootstrapSample(trainDataSet_1, trainDataSet_2)
 
-            if i < tot_batches-1:
-                np_preds[ batch_size * i : batch_size * (i+1)]  = preds
-            else:
-                np_preds[ batch_size * i :  ]  = preds
+        model_1 = learn(model_empty_1, loss, opt_1, x_boot_1, y_boot_1, device, epochs)
+        model_2 = learn(model_empty_2, loss, opt_2, x_boot_2, y_boot_2, device, epochs)
 
-            metrics_dict = audMetrics(labels, preds)
-            
-            runn_acc    += metrics_dict["accuracy"]
-            runn_prec   += metrics_dict["precision"]
-            runn_rec    += metrics_dict["recall"]
-            runn_f1     += metrics_dict["f1"]
+        metrics_dict_1 = test(model_1, testDataset_1)
+        metrics_dict_2 = test(model_2, testDataset_2)
 
-    metrics_dict["accuracy"] = runn_acc/lenDataSet
-    metrics_dict["precision"] = runn_prec/lenDataSet
-    metrics_dict["recall"] = runn_rec/lenDataSet
-    metrics_dict["f1"] = runn_f1/lenDataSet
+        sample_diff   = metrics_dict_1['accuracy'] - metrics_dict_2['accuracy']
 
-    print(f"Test model : {model.__class__.__name__} accuracy : {runn_acc/lenDataSet} f1 : {runn_f1/lenDataSet} ")
+        diffs.append(sample_diff)
 
-    return metrics_dict
+    p_value = (np.sum(diffs >= observe_diff) + np.sum(diffs <= -observe_diff)) / num_runs
+
+    return p_value
+
+    
+def test(model, testDataset):
+    # can use data loader
+
+    pass
 
 def learn(model, loss, opt, train_x, train_y, device, epochs, batch_size):
     
-    model.train()
-    model = model.double()
-
-    train_x             = toTensor(train_x)
-    train_y             = torch.from_numpy(train_y.astype(float))
     train_size 			= train_x.shape[0]
-    total_iter 			= int(np.ceil(train_size / batch_size))
+	total_iter 			= int(np.ceil(train_size / batch_size))
 
-
-    for epoch in range(epochs):
+    for epoch in epochs:
         for iter_ in range(total_iter):
-
-            if batch_size * (iter_+1) < train_size :  
-                train_batch  = train_x[ iter_ * batch_size : batch_size * (iter_ + 1) , :]
-                target_batch = train_y[ iter_ * batch_size : batch_size * (iter_ + 1) ]
-            else:
-                train_batch  = train_x[ iter_ * batch_size : , :]
-                target_batch = train_y[ iter_ * batch_size : ]
-                        
-            out = model(train_batch)
-            loss_val = loss( out, target_batch.long())
-            loss_val.backward()
-            opt.step()
-            opt.zero_grad()
             
-            print(f"Train model : {model.__class__.__name__} Epoch {epoch}/{epochs} iter  {iter_}/{total_iter}  training loss {loss_val.item()}")
 
-        # shuffling each epoch
-        index = np.arange(0, train_size)
-        np.random.shuffle(index)
-        train_x, train_y = train_x[index], train_y[index]
-        
-    return model
+    
+
+
+
+# # Define function to compute p-value for performance difference between two models
+# def compute_p_value(model1, model2, X_train, y_train, X_test, y_test, num_samples=1000):
+#     score1 = train_and_evaluate(model1, X_train, y_train, X_test, y_test)
+#     score2 = train_and_evaluate(model2, X_train, y_train, X_test, y_test)
+#     observed_diff = score1 - score2
+
+#     # Generate bootstrap samples and compute distribution of differences
+#     diffs = []
+#     for i in range(num_samples):
+#         sample_indices = np.random.choice(len(X_train), size=len(X_train), replace=True)
+#         X_sampled = X_train[sample_indices]
+#         y_sampled = y_train[sample_indices]
+#         score1_sampled = train_and_evaluate(model1, X_sampled, y_sampled, X_test, y_test)
+#         score2_sampled = train_and_evaluate(model2, X_sampled, y_sampled, X_test, y_test)
+#         diff = score1_sampled - score2_sampled
+#         diffs.append(diff)
+
+#     # Compute the p-value for the observed difference
+#     p_value = (np.sum(diffs >= observed_diff) + np.sum(diffs <= -observed_diff)) / num_samples
+#     return p_value
+
+# # Compare RNN vs. 1D CNN
+# rnn_model = create_rnn_model(input_shape)
+# cnn_model = create_cnn_model(input_shape)
+# p_value = compute_p_value(rnn_model, cnn_model, X_train, y_train, X_test, y_test)
+# print("p-value for difference between RNN and 1D CNN:", p_value)
+
+# # Compare RNN vs. audio transformer
+# transformer_model = create_transformer_model(input_shape)
+# p_value = compute_p_value(rnn_model, transformer_model, X_train, y_train, X_test, y_test)
+# print("p-value for difference between RNN and audio transformer:", p_value)
+
+# # Compare 1D CNN vs. audio transformer
+# p_value = compute_p_value(cnn_model, transformer_model, X_train, y_train, X_test, y_test)
+# print("p-value for difference between 1D CNN and audio transformer:", p_value)
--- main.py
+++ main.py
@@ -1,39 +1,84 @@
-from helpers.split_train_test import split_train_test_by_mtdata
-from helpers.train import train
-from helpers.pValAnalysis import evaluate_Pvalue
-from helpers.preprocessor import transformMelSpecByTruncate1D, transformMelSpecByTruncate2D
+from helpers.split_train_test import split_train_test_by_mtdata, split_train_test_by_user
+from helpers.train import train, test
+from helpers.tsne import  plot2D_tsne, plot3D_tsne
 from models.cnn import CNN1D, CNN2D
+from models.rnn import RNN1D
+from models.baseline import BaseLine_Linear1D
 
 import torch.nn as nn
-from torch.optim import SGD, Adam
+from torch.optim import SGD, Adam, RMSprop
+from torch.optim.lr_scheduler import StepLR
 import torch
+import numpy as np
 
+from helpers.preprocessor import transformMelSpecByTruncate1D, transformMelSpecByTruncate2D, transformMelSpecByMeanPooling1D
 import os
 
 
+# Task I
+
+# speakers = ['nicolas', 'theo' , 'jackson', 'george']
+
+# TEST_PTH  = os.path.join(os.getcwd(), 'data', 'test')
 # DATA_PATH = os.path.join(os.getcwd(), 'speech_data')
 # MDT_PATH  = os.path.join(os.getcwd(), 'SDR_metadata.tsv')
 
+# split_train_test_by_mtdata(DATA_PATH, MDT_PATH, speakers)
+
+# device  = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+# epochs = 2
+# SR     = 8000
+# batch_size = 32
+# FREQ_BANDS  = 81
+# DOWNSAMPLE_N = 15
+
+# model     = BaseLine_Linear1D(FREQ_BANDS * DOWNSAMPLE_N)
+# optimizer = SGD(model.parameters(), lr = 0.0009)
+# loss      = nn.CrossEntropyLoss()
+
+# model = train(model, loss, optimizer, None, device, epochs, transformMelSpecByMeanPooling1D, SR, batch_size, False)
+# metrics_dict, test_x, test_y, preds = test(model, TEST_PTH, loss, transformMelSpecByMeanPooling1D, device, SR)
+
+
+
+# TASK II
+TEST_PTH  = os.path.join(os.getcwd(), 'data', 'test')
+DATA_PATH = os.path.join(os.getcwd(), 'speech_data')
+MDT_PATH  = os.path.join(os.getcwd(), 'SDR_metadata.tsv')
+
+# print("creating new data splits")
 # split_train_test_by_mtdata(DATA_PATH, MDT_PATH)
+# print("splits created")
 
 device  = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-
-transform1 = transformMelSpecByTruncate1D
-transform2 = transformMelSpecByTruncate2D
-
-model1 = CNN1D()
-model2 = CNN2D()
-
-opt1 = Adam(model1.parameters(), lr = 0.001) 
-opt2 = Adam(model2.parameters(), lr = 0.001) 
-
-loss  = nn.CrossEntropyLoss()
 epochs = 10
+SR     = 8000
 batch_size = 32
-num_runs = 20
-SR = 8000
 
-pvalue = evaluate_Pvalue(model1, model2, transform1, transform2, opt1, opt2, 
-    loss, SR, batch_size, epochs, num_runs, device)
+model = CNN1D()
+optimizer = Adam(model.parameters(), lr = 0.001)
+scheduler = StepLR(optimizer, step_size=30, gamma=0.1)
+loss  = nn.CrossEntropyLoss()
 
-print(pvalue)
+model = train(model, loss, optimizer, None, device, epochs, transformMelSpecByTruncate1D, SR, batch_size, True)
+metrics_dict, test_x, test_y, preds = test(model, TEST_PTH, loss, transformMelSpecByTruncate1D, device, SR)
+# plot3D_tsne(test_x, test_y, preds, "CNN1D_epoch_2_3D.jpg")
+
+
+
+
+# # model = CNN2D()
+# # optimizer = SGD(model.parameters(), lr = 0.01)
+# # loss  = nn.CrossEntropyLoss()
+
+# # model = train(model, loss, optimizer, None, device, epochs, transformMelSpecByTruncate2D, SR, batch_size, False)
+# # test(model, TEST_PTH, loss, transformMelSpecByTruncate2D, device, SR)
+
+
+
+# # model = RNN1D(input_size=630, hidden_size=32 , num_layers=2 , device=device)
+# # optimizer = Adam(model.parameters(), lr = 0.01)
+# # loss  = nn.CrossEntropyLoss()
+
+# # model = train(model, loss, optimizer, None, device, epochs, transformMelSpecByTruncate1D, SR, batch_size, False)
+# # test(model, TEST_PTH, loss, transformMelSpecByTruncate1D, device, SR)
